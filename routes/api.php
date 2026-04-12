00d505f90c0e8b4790fe7ac295a45ce7c69e6c1a
valdijunior <valdi.ads@gmail.com>
2026-04-12 16:50:13 -0300

    feat: remove código morto.

--- routes/api.php
+++ routes/api.php
@@ -1,25 +1,5 @@
 <?php
 
-// use App\Http\Controllers\Api\V1\AuthController;
-// use Illuminate\Support\Facades\Route;
-
-// Route::group([
-//     'middleware' => 'api',
-//     'prefix' => 'auth'
-
-// ], function ($router) {
-
-//     Route::post('login', [AuthController::class, 'login']);
-//     Route::post('logout', [AuthController::class, 'logout']);
-//     Route::post('refresh', [AuthController::class, 'refresh']);
-//     Route::post('me', [AuthController::class, 'me']);
-// });
-
-// // Rotas protegidas (precisam de token)
-// Route::middleware('auth:api')->group(function () {
-
-// });
-
 use App\Http\Controllers\Api\V1\AuthController;
 use Illuminate\Support\Facades\Route;
 
