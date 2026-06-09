69330473181d0d298e640ab5a07ef80c38caee4a
valdijunior <valdi.ads@gmail.com>
2026-06-08 22:05:34 -0300

    feat: adiciona softDelete ao model Patient

--- app/Models/Patient.php
+++ app/Models/Patient.php
@@ -8,12 +8,13 @@ use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphOne;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 //escreva todos os campos da tabela patients no array do Fillable
 #[Fillable(['name', 'friendly_id', 'birth_date', 'gender', 'cpf', 'rg', 'email', 'phone_number', 'emergency_contact'])]
 class Patient extends Model
 {
-    use BelongsToTenant, HasFriendlyId, HasFactory;
+    use BelongsToTenant, HasFriendlyId, SoftDeletes,HasFactory;
 
     public function address(): MorphOne
     {
