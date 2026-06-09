4c250b6aab9b2a8fe1c9c536f5011c533edd2b31
valdijunior <valdi.ads@gmail.com>
2026-06-08 22:10:26 -0300

    feat: cria request para excluir lista de pacientes

---
+++ app/Http/Requests/Patients/BulkDeletePatientRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests\Patients;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class BulkDeletePatientRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return $this->user() !== null;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'ids' => ['required', 'array', 'min:1'],
+            'ids.*' => ['required', 'integer', 'exists:patients,id'],
+        ];
+    }
+}
